b91cf9b4876cdc4c0397e3fb94e305c933ed138b
braitsch <stephen@braitsch.io>
2018-08-25 11:32:51 -0700

    updating dependencies and migrating from jade to pug templating engine

--- app.js
+++ app.js
@@ -6,20 +6,19 @@
 
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
+var server  = require('http').createServer(app);
 
 // create the single instance of socket.io that will be shared across all applications //
-global.io = require('socket.io')(http);
+global.io = require('socket.io').listen(server);
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/app/server/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
 app.use(express.static(__dirname + '/app/public'));
 
 require('./app/server/routes')(app);
 
-http.listen(app.get('port'), function()
-{
-	console.log('Express server listening on port', app.get('port'));
+server.listen(app.get('port'), function(){
+	console.log('Express app listening at http://%s:%s', server.address().address, server.address().port);
 });
