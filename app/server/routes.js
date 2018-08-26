736991a76f3cec3bbfa0de0c8a5f0d3601868e0f
braitsch <stephen@braitsch.io>
2018-08-26 16:47:18 -0700

    disabling 404 * because it is interfering with certbot

--- app/server/routes.js
+++ app/server/routes.js
@@ -3,15 +3,8 @@ require('./modules/doodle-socket');
 
 module.exports = function(app) {
 
-	app.get('/', canvas);
-	app.get('*', nfnd);
+	app.get('/', function(req, res){
+		res.render('canvas', { title: 'Socket.IO Doodle'});
+	});
 
-};
-
-function canvas(req, res){
-	res.render('canvas', { title: 'Socket.IO Doodle'});
-}
-
-function nfnd(req, res) {
-	res.render('404', { title: 'Page Not Found'});
 };
